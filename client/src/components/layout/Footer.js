12085ee831b868543a321fde88530c258d64ef9e
MaciejReimann <maciej.reimann@gmail.com>
2018-08-10 07:26:21 +0200

    footer, navbar and landing created

---
+++ client/src/components/layout/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const Footer = () => {
+  return (
+footer.bg-dark.text-white.mt-5.p-4.text-center 
+  )
+}
+
+export default Footer;
+1235    098
