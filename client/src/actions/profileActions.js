b5fbc3e0306661da05a377a22af988a254d931e1
MaciejReimann <maciej.reimann@gmail.com>
2018-08-17 19:40:15 +0200

    CreateProfile Functionality added

--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -20,16 +20,30 @@ export const getCurrentProfile = () => dispatch => {
     )
 };
 
+// Create Profile
+export const createProfile = (profileData, history) => dispatch => {
+  axios
+    .post('/api/profile', profileData)
+    .then(res => history.push('/dashboard'))
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    )
+};
+
+
 // Profile loading
 export const setProfileLoading = () => {
   return {
     type: PROFILE_LOADING
   }
-}
+};
 
 // Clear Profile
 export const clearCurrentProfile = () => {
   return {
     type: CLEAR_CURRENT_PROFILE
   }
-}
+};
