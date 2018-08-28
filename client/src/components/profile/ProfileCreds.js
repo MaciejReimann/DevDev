ce137a80f8ecb4a6a02bda5ada1a19030cc4a2de
MaciejReimann <maciej.reimann@gmail.com>
2018-08-28 13:12:35 +0200

    displayin profile by handle

---
+++ client/src/components/profile/ProfileCreds.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+class ProfileCreds extends React.Component {
+  render () {
+    return (
+      <div>
+        <h1>
+          TODO: PROFILE CREDS
+        </h1>
+      </div>
+    )
+  }
+}
+
+export default ProfileCreds;
